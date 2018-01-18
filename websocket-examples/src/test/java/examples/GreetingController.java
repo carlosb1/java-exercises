36d42e29c172921ca952dd1ff225982cd043ad7c
carlosb <mcflurry0@gmail.com>
2018-01-18 23:49:55 +0100

    Example for webscockets

---
+++ websocket-examples/src/test/java/examples/GreetingController.java
@@ -0,0 +1,17 @@
+package examples;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/examples")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+
+}
